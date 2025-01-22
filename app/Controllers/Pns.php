d751729fbbb3232fef9edfd934444abc1af975f7
danuri <danualbantani@gmail.com>
2025-01-22 09:48:25 +0700

    Update Pns.php

--- app/Controllers/Pns.php
+++ app/Controllers/Pns.php
@@ -33,7 +33,7 @@ class Pns extends BaseController
     
     public function ipasn($nip)
     {
-        // GET /pns/data-utama/{nipBaru}
+        // GET /pns/nilaiipasn/{nipBaru}
 
         $client = service('curlrequest');
         $cache = service('cache');
@@ -70,5 +70,59 @@ class Pns extends BaseController
         ]);
 
         echo $response->getBody();
+    }
+
+    public function datapasangan($nip)
+    {
+        // GET /pns/data-pasangan/{nipBaru}
+
+        $client = service('curlrequest');
+        $cache = service('cache');
+
+        $response = $client->request('GET', getenv('wso.apisiasn.endpoint').'/pns/data-pasangan/'.$nip, [
+            'headers' => [
+                'Auth'              => 'bearer '.getenv('wso.auth.token'),
+                'Authorization'     => 'Bearer '.service('cache')->get('oauth2.token'),
+            ],
+            'verify' => false
+        ]);
+
+        return $this->response->setJSON($response->getBody());
+    }
+
+    public function dataanak($nip)
+    {
+        // GET /pns/data-anak/{nipBaru}
+
+        $client = service('curlrequest');
+        $cache = service('cache');
+
+        $response = $client->request('GET', getenv('wso.apisiasn.endpoint').'/pns/data-anak/'.$nip, [
+            'headers' => [
+                'Auth'              => 'bearer '.getenv('wso.auth.token'),
+                'Authorization'     => 'Bearer '.service('cache')->get('oauth2.token'),
+            ],
+            'verify' => false
+        ]);
+
+        return $this->response->setJSON($response->getBody());
+    }
+
+    public function dataortu($nip)
+    {
+        // GET /pns/data-ortu/{nipBaru}
+
+        $client = service('curlrequest');
+        $cache = service('cache');
+
+        $response = $client->request('GET', getenv('wso.apisiasn.endpoint').'/pns/data-ortu/'.$nip, [
+            'headers' => [
+                'Auth'              => 'bearer '.getenv('wso.auth.token'),
+                'Authorization'     => 'Bearer '.service('cache')->get('oauth2.token'),
+            ],
+            'verify' => false
+        ]);
+
+        return $this->response->setJSON($response->getBody());
     }
 }
