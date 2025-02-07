97cb35f4c379be6159305012cdd2693986b98440
danuri <danualbantani@gmail.com>
2025-02-07 11:32:32 +0700

    Create Anomali.php

---
+++ app/Controllers/Anomali.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Anomali extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+
+    public function jenis()
+    {
+        $client = service('curlrequest');
+
+        $response = $client->request('GET', 'https://api-siasn.bkn.go.id/siasn-instansi/anomali/jenisanomaliinstitusi', [
+            'headers' => [
+                'Accept'        => 'application/json',
+                'Content-Type' => 'application/json',
+                'Origin' => 'https://siasn-instansi.bkn.go.id',
+                'referer' => 'https://siasn-instansi.bkn.go.id/tampilanData',
+                'Authorization'     => 'Bearer '.getenv('wso.auth.token'),
+            ],
+            'debug' => true,
+            'verify' => false
+        ]);
+
+        return $response->getBody();
+    }
+
+    public function delete($id)
+    {
+        // DELETE /angkakredit/delete/{idRiwayatAngkaKredit}
+    }
+
+    public function save()
+    {
+        // POST /angkakredit/save
+    }
+}
