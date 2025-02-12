59de18173f7e2c26de8c79e28b8a099280c8eb8b
danuri <danualbantani@gmail.com>
2025-02-12 10:48:38 +0700

    Update Riwayat.php

--- app/Controllers/Riwayat.php
+++ app/Controllers/Riwayat.php
@@ -11,4 +11,42 @@ class Riwayat extends BaseController
     {
         //
     }
+
+    public function pendidikan($nip)
+    {
+        // GET /pns/rw-pendidikan/{nipBaru}
+
+        $client = service('curlrequest');
+        $cache = service('cache');
+
+        $response = $client->request('GET', getenv('wso.apisiasn.endpoint').'/pns/rw-pendidikan/'.$nip, [
+            'headers' => [
+                'Auth'              => 'bearer '.getenv('wso.auth.token'),
+                'Authorization'     => 'Bearer '.service('cache')->get('oauth2.token'),
+            ],
+            'verify' => false
+        ]);
+
+        // echo $response->getBody();
+        return $this->response->setJSON($response->getBody());
+    }
+
+    public function diklat($nip)
+    {
+        // GET /pns/rw-diklat/{nipBaru}
+
+        $client = service('curlrequest');
+        $cache = service('cache');
+
+        $response = $client->request('GET', getenv('wso.apisiasn.endpoint').'/pns/rw-diklat/'.$nip, [
+            'headers' => [
+                'Auth'              => 'bearer '.getenv('wso.auth.token'),
+                'Authorization'     => 'Bearer '.service('cache')->get('oauth2.token'),
+            ],
+            'verify' => false
+        ]);
+
+        // echo $response->getBody();
+        return $this->response->setJSON($response->getBody());
+    }
 }
