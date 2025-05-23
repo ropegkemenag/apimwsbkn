7c4fb64642770793051c5205a84f9b2ce148fd6d
danuri <danualbantani@gmail.com>
2025-05-23 14:19:24 +0700

    Update Pengadaan.php

--- app/Controllers/Pengadaan.php
+++ app/Controllers/Pengadaan.php
@@ -220,7 +220,7 @@ class Pengadaan extends BaseController
                 'jenis_pegawai_id' => $row->jenis_pegawai_id,
             ];
 
-            $model->insert($param);
+            $model->save($param);
         }
     }
     
