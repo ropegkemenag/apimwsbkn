397dd08a9563c58c24f2e42d6eaeaeccc7aa8b66
danuri <danualbantani@gmail.com>
2025-08-07 17:18:36 +0700

    paruh waktu

--- app/Controllers/Casn.php
+++ app/Controllers/Casn.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 use App\Models\CasnStatSatkerModel;
+use App\Models\ParuhModel;
 
 class Casn extends BaseController
 {
@@ -130,5 +131,87 @@ class Casn extends BaseController
         $check = $model->find($id);
 
         return ($check)?true:false;
+    }
+
+    public function paruhwaktu($page)
+    {
+        $client = service('curlrequest');
+
+        $response = $client->request('GET', 'https://api-siasn.bkn.go.id/perencanaan/formasi_paruh_waktu/list_paruh_waktu', [
+            'headers' => [
+                'Accept'        => 'application/json',
+                'Content-Type' => 'application/json',
+                'Origin' => 'https://dashboard-sscasn.bkn.go.id',
+                'referer' => 'https://dashboard-sscasn.bkn.go.id/',
+                'Authorization'     => 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJBUWNPM0V3MVBmQV9MQ0FtY2J6YnRLUEhtcWhLS1dRbnZ1VDl0RUs3akc4In0.eyJleHAiOjE3NTQ1OTk2MjgsImlhdCI6MTc1NDU1NjQzNywiYXV0aF90aW1lIjoxNzU0NTU2NDI4LCJqdGkiOiJhMjU2MzY5ZS05YTg4LTQyN2MtOGQ5MC0xODhhN2RkZGVhNmEiLCJpc3MiOiJodHRwczovL3Nzby1zaWFzbi5ia24uZ28uaWQvYXV0aC9yZWFsbXMvcHVibGljLXNpYXNuIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6ImFmMjQxODExLTFhZTUtNDc1OS05NzBlLTRlMjI2OTBiMzM5NyIsInR5cCI6IkJlYXJlciIsImF6cCI6ImJrbi1zaWFzbi1wZXJlbmNhbmFhbiIsIm5vbmNlIjoiMWMzYmRiZWUtZmU3ZC00MmMxLTk3ODktNjM2NTY1YWM2ODA4Iiwic2Vzc2lvbl9zdGF0ZSI6ImFhNTA1YzEwLWEzOWMtNGRjNy05NWY4LTk4MmZhYzE3ODAzNyIsImFjciI6IjAiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovL3BlcmVuY2FuYWFuLXNpYXNuLmJrbi5nby5pZCIsImh0dHBzOi8vcGVyZW5jYW5hYW4tc2lhc24uYmtuLmdvLmlkIiwiaHR0cDovL2xvY2FsaG9zdDo0MjAwIiwiaHR0cDovL2xvY2FsaG9zdDo4MDAwIiwiaHR0cDovL2xvY2FsaG9zdDozMDAwIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJyb2xlOnNpYXNuLWluc3RhbnNpOnBlcmVtYWphYW46b3BlcmF0b3IiLCJyb2xlOmltdXQtaW5zdGFuc2k6bW9uaXRvcmluZyIsInJvbGU6c2lhc24taW5zdGFuc2k6cGVyZW5jYW5hYW46dXN1bC1wZW5nYWxpaGFuLWZvcm1hc2kiLCJyb2xlOnNpYXNuLWluc3RhbnNpOnBlcmVuY2FuYWFuOmluc3RhbnNpLW9wZXJhdG9yLWluZm9qYWIiLCJyb2xlOnNpYXNuLWluc3RhbnNpOnBlcmVtYWphYW46VFRFIiwicm9sZTpzaWFzbi1pbnN0YW5zaTpwZXJlbmNhbmFhbjppbnN0YW5zaS1tb25pdG9yLXBlcmVuY2FuYWFuLWtlcGVnYXdhaWFuIiwicm9sZTpzaWFzbi1pbnN0YW5zaTpwZW5nYWRhYW46YXBwcm92YWwiLCJyb2xlOnNpYXNuLWluc3RhbnNpOnBlbmdhZGFhbjpvcGVyYXRvci1za3BucyIsInJvbGU6c2lhc24taW5zdGFuc2k6cGVyZW1hamFhbjpyZWtvbiIsInJvbGU6ZGlzcGFrYXRpOmluc3RhbnNpOnR0ZSIsInJvbGU6c2lhc24taW5zdGFuc2k6a3A6b3BlcmF0b3IiLCJyb2xlOmltdXQtaW5zdGFuc2k6YWRtaW4iLCJyb2xlOm1hbmFqZW1lbi13czpkZXZlbG9wZXIiLCJvZmZsaW5lX2FjY2VzcyIsInJvbGU6c2lhc24taW5zdGFuc2k6cGk6cGVuZ2FsaWhhbi1rbDpvcGVyYXRvciIsInJvbGU6c2lhc24taW5zdGFuc2k6YmF0YWxuaXA6b3BlcmF0b3IiLCJyb2xlOnNpYXNuLWluc3RhbnNpOnBlcmVuY2FuYWFuOmluc3RhbnNpLW9wZXJhdG9yLXBlbWVudWhhbi1rZWItcGVnYXdhaSIsInVtYV9hdXRob3JpemF0aW9uIiwicm9sZTpzaWFzbi1pbnN0YW5zaTpwZXJlbmNhbmFhbjppbnN0YW5zaS1vcGVyYXRvci1ldmFqYWIiLCJyb2xlOnNpYXNuLWluc3RhbnNpOnBlbmdhZGFhbjpwYXJhZiIsInJvbGU6c2lhc24taW5zdGFuc2k6c2trOm9wZXJhdG9yIiwicm9sZTpzaWFzbi1pbnN0YW5zaTpwZXJlbWFqYWFuOmFwcHJvdmFsIiwicm9sZTpzaWFzbi1pbnN0YW5zaTpwZXJlbmNhbmFhbjppbnN0YW5zaS1vcGVyYXRvci1zb3RrIiwicm9sZTpzaWFzbi1pbnN0YW5zaTpwZXJlbmNhbmFhbjp1c3VsLXJpbmNpYW4tZm9ybWFzaSIsInJvbGU6ZGlzcGFrYXRpOmluc3RhbnNpOm9wZXJhdG9yIiwicm9sZTpzaWFzbi1pbnN0YW5zaTpwZW5nYWRhYW46b3BlcmF0b3IiLCJyb2xlOnNpYXNuLWluc3RhbnNpOnBpOnBlbmdhbGloYW4ta2w6YXBwcm92YWwiLCJyb2xlOnNpYXNuLWluc3RhbnNpOmJhdGFsbmlwOmFwcHJvdmFsIiwicm9sZTpzaWFzbi1pbnN0YW5zaTphZG1pbi10ZW1wbGF0ZSIsInJvbGU6c2lhc24taW5zdGFuc2k6cGVyZW5jYW5hYW46aW5zdGFuc2ktb3BlcmF0b3Itc3RhbmRhci1rb21wLWphYiIsInJvbGU6c2lhc24taW5zdGFuc2k6cGVyZW5jYW5hYW46aW5zdGFuc2ktcGVuZXRhcGFuLXNvdGsiLCJyb2xlOnNpYXNuLWluc3RhbnNpOnBlcmVtYWphYW46cGFyYWYiLCJyb2xlOnNpYXNuLWluc3RhbnNpOnByb2ZpbGFzbjp2aWV3cHJvZmlsIiwicm9sZTpzaWFzbi1pbnN0YW5zaTphZG1pbjphZG1pbiIsInJvbGU6c2lhc24taW5zdGFuc2k6cGVyZW5jYW5hYW46aW5zdGFuc2ktdmFsaWRhdG9yLXN0YW5kYXIta29tcC1qYWIiXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoiREFOVVJJIERBTlVSSSIsInByZWZlcnJlZF91c2VybmFtZSI6IjE5ODcwNzIyMjAxOTAzMTAwNSIsImdpdmVuX25hbWUiOiJEQU5VUkkiLCJmYW1pbHlfbmFtZSI6IkRBTlVSSSIsImVtYWlsIjoiZGFudWFsYmFudGFuaUBnbWFpbC5jb20ifQ.OzsSag9YSnxyTxaLsY4Pmoibn-OIjY5WIgxJyEHbPSqG_yHcjSLy8s3EZkxobuZnWZLY9MfVZL1QOeFHvcZ3O_nwhoyNXHU8SzmVSDshjYdnMJV6yQ7tX8VHN5i9HgQF9Veoss0a1j6VaEIXBePxqnYpjHFwTlKxmO8YymYddbzEO2esgk-dwrhYu9F-XgzX0Oo94oOWHSmhPGYQQ3zED-uTr5GFfM5IPTYtT1GiIb9zIrAaF7jeq3gIlJmnPJLd7jpLvqgN_ykKyt3xddNAyognDlr9WqvvPGbHUvUStivrvM4h3lOJLxmFQwq-lHJtdlTe5EfIFqAd5NJkYTkpHQ',
+            ],
+            'query' => [
+                'instansi_id' => 'A5EB03E23BFBF6A0E040640A040252AD',
+                'page' => $page
+            ],
+            'debug' => true,
+            'verify' => false
+        ]);
+
+        $list = $response->getBody();
+
+        $lists = json_decode($list);
+
+        $model = new ParuhModel;
+        foreach($lists->data->records as $row){
+            $param = [
+                'nik' => $row->nik,
+                'nama' => $row->nama,
+                'jenis_kelamin' => $row->jenis_kelamin,
+                'cepat_kode_sscn' => $row->cepat_kode_sscn,
+                'instansi_sscn' => $row->instansi_sscn,
+                'jenis_pengadaan_id' => $row->jenis_pengadaan_id,
+                'jabatan_siasn_id' => $row->jenis_pengadaan_id,
+                'kode_jabatan_sscn' => $row->kode_jabatan_sscn,
+                'jabatan_sscn' => $row->jabatan_sscn,
+                'kode_pendidikan_sscn' => $row->kode_pendidikan_sscn,
+                'pendidikan_sscn' => $row->pendidikan_sscn,
+                'lokasi_formasi_sscn' => $row->lokasi_formasi_sscn,
+                'usia_saat_daftar_sscn' => $row->usia_saat_daftar_sscn,
+                'tahap_sscn' =>  $row->tahap_sscn,
+                'is_tampungan_sscn' => $row->is_tampungan_sscn,
+                'cepat_kode_nonasn' => $row->cepat_kode_nonasn,
+                'instansi_non_asn' => $row->instansi_non_asn,
+                'unor_id_nonasn' => $row->unor_id_nonasn,
+                'unor_nama_nonasn' => $row->unor_nama_nonasn,
+                'pendidikan_nama_nonasn' => $row->pendidikan_nama_nonasn,
+                'kode_jabatan_nonasn' => $row->kode_jabatan_nonasn,
+                'jabatan_nonasn' => $row->jabatan_nonasn,
+                'status_aktif_nonasn' => $row->status_aktif_nonasn,
+                'updated_at' => $row->updated_at,
+                'jabatan_rincian_id' => $row->jabatan_rincian_id,
+                'jabatan_rincian_nama' => $row->jabatan_rincian_nama,
+                'sub_jabatan_rincian_id' => $row->sub_jabatan_rincian_id,
+                'sub_jabatan_rincian_nama' => $row->sub_jabatan_rincian_nama,
+                'rincian_pendidikan_id' => $row->rincian_pendidikan_id,
+                'rincian_pendidikan_nama' => $row->rincian_pendidikan_nama,
+                'rincian_tk_pendidikan' => $row->rincian_tk_pendidikan,
+                'unit_penempatan_id' => $row->unit_penempatan_id,
+                'unit_penempatan_nama' => $row->unit_penempatan_nama,
+                'is_terdata_nonasn' => $row->is_terdata_nonasn,
+                'status_prioritas' => $row->status_prioritas,
+                'is_usul' => $row->is_usul,
+                'alasan_tolak' => $row->alasan_tolak
+            ];
+
+            // if($this->checkId($row->formasi_id)){
+            //     $save = $model->save($param);
+            // }else{
+            // }
+            $save = $model->insert($param);
+        }
+
+        $page = $page+1;
+        if($lists->data->pagination->last_page < 197){
+            return redirect()->to('casn/paruhwaktu/'.$page);
+        }else{
+            echo 'Done';
+        }
+
     }
 }
